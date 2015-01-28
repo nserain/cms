681706b1c727a3fcce9d9cf4f5d948f755a6e995
Leiah <leah.volza.turi@gmail.com>
2015-01-28 11:29:47 +0100

    Update accueil.php

--- web/accueil.php
+++ web/accueil.php
@@ -3,3 +3,4 @@
 		Les chatons et les animes sont les passions de ma vie!</p>
 	<img src="images/Chaton-roux.jpg" width="160" height="90"><br/>
 	<img src="images/Fd.jpg" width="192" height="120"><br/>
+	<!-- un commentaire-->
